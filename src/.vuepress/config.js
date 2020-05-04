37cb5c629b33aaf96f5c8e7b66d85aa19b76dabe
Yuka Aoki <smilist.kuyu@gmail.com>
2020-05-05 02:16:10 +0900

    home, about 修正、class,contact 追加

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,87 +1,25 @@
 module.exports = {
-    title: 'ニコプロ',
+    title: '鹿児島のICT＆プログラミング教室 ニコプロ',
     description: '小中学生向け！鹿児島のICT＆プログラミング教室です。Scratch（スクラッチ）やLEGOロボットでICT＆プログラミングを楽しく学ぼう！',
     locales: {
         '/': {
             lang: 'ja-JP',
         },
     },
+    plugins: [
+        "@vuepress/blog",
+    ],
     head: [
-        ['link', { rel: 'icon', type: 'image/png', href: 'img/favicon.png' }],
-        ['link', { href: 'css/style.css', rel: 'stylesheet' }]
+        ['link', { rel: 'icon', type: 'image/png', href: '/img/favicon.png' }],
+        ['link', { href: '/css/style.css', rel: 'stylesheet' }]
     ],
     themeConfig: {
+        logo: '/img/logo.png',
         nav: [
             { text: 'ニコプロって？', link: '/about/' },
-            { text: 'ブログ', link: '/posts/' },
+            { text: '教室案内   ', link: '/class/' },
+            { text: 'ブログ', link: '/_posts/' },
+            { text: 'お問い合わせ', link: '/contact/' },
         ],
-    }
-    // // Theme to use
-    // theme: 'meteorlxy',
-    // themeConfig: {
-    //     lang: 'ja-JP',
-    //     personalInfo: {
-    //         // Nickname
-    //         nickname: 'nicopro',
-    //         description: 'ニコプロ代表青木です！',
-    //         email: 'info@nicopro.site',
-    //         avatar: 'img/profile.jpg',
-    //         sns: {
-    //             instagram: {
-    //                 account: 'nicopro.smilist',
-    //                 link: 'https://www.instagram.com/nicopro.smilist',
-    //             },
-    //             facebook: {
-    //                 account: 'nicopro.smilist',
-    //                 link: 'https://business.facebook.com/nicopro.smilist/',
-    //             },
-    //             twitter: {
-    //                 account: '@smilist_kuyu',
-    //                 link: 'https://twitter.com/smilist_kuyu',
-    //             },
-    //             github: {
-    //                 account: 'nicopro-kuyu',
-    //                 link: 'https://github.com/nicopro-kuyu/',
-    //             },
-    //         },
-    //     },
-
-    //     header: {
-    //         background: {
-    //             useGeo: true,
-    //         },
-    //         showTitle: true,
-    //     },
-
-    //     footer: {
-    //         poweredBy: true,
-    //         poweredByTheme: true,
-    //     },
-
-    //     infoCard: {
-    //         headerBackground: {
-    //             useGeo: true,
-    //         },
-    //     },
-
-    //     lastUpdated: true,
-
-    //     nav: [
-    //         { text: 'ニコプロって？', link: '/about/' },
-    //         { text: 'ブログ', link: '/posts/' },
-    //     ],
-
-    //     comments: false,
-
-    //     pagination: {
-    //         perPage: 5,
-    //     },
-
-    //     defaultPages: {
-    //         home: true,
-    //         posts: true,
-    //     },
-
-    // },
-
+    },
 }
---
+++ src/contact/index.md
@@ -0,0 +1,30 @@
+# お問い合わせ
+お問い合わせはこちらからどうぞ！ 2営業日以内にご連絡いたします。
+
+LINEでもメッセージのやり取り可能です。 お気軽にご連絡ください。
+
+<a href="http://nav.cx/4chnsB5"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"></a>
+
+<form name="contact" method="post" action="" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <dl>
+        <dt><label for="name">お名前</label></dt>
+        <dd><input type="text" id="name" /></dd>
+        <dt><label for="email">メールアドレス</label></dt>
+        <dd><input type="email" id="email" /></dd>
+        <dt><label>お問い合わせの種類</label></dt>
+        <dd class="selectbox">
+          <select name="item">
+            <option value="">お問い合わせの種類をご選択ください</option>
+            <option value="ご質問">ご質問</option>
+            <option value="メッセージ">メッセージ</option>
+            <option value="その他">その他</option>
+          </select>
+        </dd>
+        <dt><label>メッセージ</label></dt>
+        <dd><textarea name="message" cols="40" rows="10"></textarea></dd>
+      </dl>
+      <div class="btn"><button type="submit" class="submit-btn">送信</button></div>
+    </form>
+
